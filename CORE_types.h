31105d5c32b2c65741bcc38884918a67183c36f0
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-22 15:33:21 -0500

    Update CORE_types.h

--- CORE_types.h
+++ CORE_types.h
@@ -12,11 +12,13 @@
 
 extern int _CGE;
 
-#  define CoreConErr    2 // Starts at 2 since Failure is 1
-#  define FileConErr    3
-#  define NotDumped     4
-#  define Failure       1
-#  define Success       0
+#  define CoreConErr         2 // Starts at 2 since Failure is 1
+#  define FileConErr         3
+#  define NotDumped          4
+#  define SameNameErr        5
+#  define FoundInOtherFile   6
+#  define Failure            1
+#  define Success            0
 
 #define CTYPES() \
 	printf("TYPES: \n%s",(_CGE == 0) ? "\tCoreConErr - Err Status 1\n\tFileConErr - Err Status 2\n\tNotDumped - Err Status 3\n" : "\tFailure - Exit Status 1\n\tSuccess - Exit Status 0");
