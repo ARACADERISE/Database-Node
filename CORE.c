8a1a299cbef98a50c549a7a4c1952e380299757b
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-22 14:56:38 -0500

    Update CORE.c

--- CORE.c
+++ CORE.c
@@ -11,36 +11,43 @@
 
 char *DatabaseNodeName;
 int _CGE;
-// This will append names created by the user
-static char * DbNodeNames[] = {
+// This is Default db names
+static char * DefDbNodeNames[] = {
 	// DEFAULT NAMES(14-17)
 	"Default01", // Default db, user can assign era type
 	"Tyme7", // Default db, user can assign era type
 	"FileReader", // Defaut db, can only read files
 	"FileWriter" // Default db, can only write files
-
-	// User defined Database Nodes defined from index 4+
 };
+// This is user created name
+// This is needed due to the fact if we set calloc(500, sizeof(DefDbNodeNames)*80)
+// To DefDbNodeNames, it appends all user created names
+// To the begginning
+char *DbNames;
 
 void SetupDatabaseNode(
-	char *DatabaseNode,
+	char DatabaseNode[50],
 	bool CoreGenereatedErrs,
 	bool NodeCanRead,
 	char *Era
 ) {
 	static int InitId = 0;
 	static int DefDb = 0;
+	static int InitUpd = 0;
 	DatabaseNodeset * NodeSetup = (DatabaseNodeset *) malloc(sizeof(DatabaseNodeset));
 	
 	if(DefDb != -1) {
-		if(strcmp(DatabaseNode,"default "/*Space needed due to /0 at end of string*/)) {
-			DatabaseNode = DbNodeNames[DefDb];
-			if(DefDb != 3) {
-				DefDb++;
-			}
-			else {
+		if(strcmp(DatabaseNode,"default" /*Space needed due to /0 at end of string*/) == 0) {
+			DatabaseNode = DefDbNodeNames[DefDb];
+			if(DefDb != 3)
+				DefDb ++;
+			else
 				DefDb = -1;
-			}
+			printf("Yep");
+		} else {
+			DbNames = calloc(500, sizeof(DbNames)*80);
+			strcpy(&DbNames[InitUpd],DatabaseNode);
+			InitUpd++;
 		}
 	} else {
 		printf("You can only assign 4 default Database Nodes\n");
