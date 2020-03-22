5a0e2ca65bd323c31ba284da169774539664bd1c
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-22 15:44:32 -0500

    Update CORE_types.h

--- CORE_types.h
+++ CORE_types.h
@@ -15,13 +15,25 @@ extern int _CGE;
 #  define CoreConErr         2 // Starts at 2 since Failure is 1
 #  define FileConErr         3
 #  define NotDumped          4
-#  define SameNameErr        5
-#  define FoundInOtherFile   6
+#  define FoundInOtherFile   5
 #  define Failure            1
 #  define Success            0
 
 #define CTYPES() \
 	printf("TYPES: \n%s",(_CGE == 0) ? "\tCoreConErr - Err Status 1\n\tFileConErr - Err Status 2\n\tNotDumped - Err Status 3\n" : "\tFailure - Exit Status 1\n\tSuccess - Exit Status 0");
 
-#define RETURNERR(TYPE) \
-	printf("%s",(TYPE == CoreConErr) ? "Yes" : "No")
+#define RETURNERRINFO(TYPE) \
+	if(TYPE == CoreConErr) \
+		printf("Error connection with core. \n\tReturn Exit status %d",CoreConErr); \
+	else if(TYPE == FileConErr) \
+		printf("Error connecting/finding file. Return Exit status %d",FileConErr); \
+	else if(TYPE == FoundInOtherFile) \
+		printf("The string/number/character was found in another file. \n\tReturn Exit status %d",FoundInOtherFile); \
+	else if(TYPE == Failure) \
+		printf("Failed to compile successfully. \n\tReturn Exit status %d",Failure); \
+	else if(TYPE == Success) \
+		printf("Program compiled successfully. \n\tReturn Exit status %d",Success); \
+	else { \
+		printf("Unknown return type"); \
+		abort(); \
+	}
