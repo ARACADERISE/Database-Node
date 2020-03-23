1513d28f93a991256cdd01e0f93a65bfa0796b61
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-23 17:51:50 -0500

    Update dataparser.py

--- Python/dataparser.py
+++ Python/dataparser.py
@@ -7,6 +7,7 @@ import os, json
 
 listed_ = []
 node_names = []
+Action = []
 IdNumber = []
 d = 1
 
@@ -25,8 +26,9 @@ for i in range(len(node_names)):
 	IdNumber.append(node_names[i][index])
 	node_names[i] = node_names[i].replace('\n','')
 	node_names[i] = node_names[i].replace(IdNumber[i],'')
+	Action.append(node_names[i])
 	node_names[i] = node_names[i].replace('Added Database Node ','')
-	data_ = {'NodeNames':node_names,'NodeIds':IdNumber}
+	data_ = {'Actions':Action,'NodeNames':node_names,'NodeIds':IdNumber}
 	d+= 1
 
 with open('NodeData.json','w') as NodeData:
