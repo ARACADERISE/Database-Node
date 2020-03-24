d6f9d61a399dd87ce53af1c2af2ea942f0aeef85
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-24 16:01:17 -0500

    Update dataparser.py

--- Python/dataparser.py
+++ Python/dataparser.py
@@ -15,30 +15,31 @@ for i in os.listdir(os.path.abspath('.')):
 	if 'Node Information #' in i:
 		listed_.append(i)
 
-for i in range(len(listed_)):
-	data = open(listed_[i],'r').read()
-	node_names.append(data)
+if len(listed_) != 0:
+	for i in range(len(listed_)):
+		data = open(listed_[i],'r').read()
+		node_names.append(data)
 
-for i in range(len(node_names)):
-	if str(d) in node_names[i]:
-		index = node_names[i].find(str(d))
-	
-	IdNumber.append(node_names[i][index])
-	node_names[i] = node_names[i].replace('\n','')
-	node_names[i] = node_names[i].replace(IdNumber[i],'')
-	Action.append(node_names[i])
-	node_names[i] = node_names[i].replace('Added Database Node ','')
-	data_ = {'Actions':Action,'NodeNames':node_names,'NodeIds':IdNumber}
-	d+= 1
+	for i in range(len(node_names)):
+		if str(d) in node_names[i]:
+			index = node_names[i].find(str(d))
+		
+		IdNumber.append(node_names[i][index])
+		node_names[i] = node_names[i].replace('\n','')
+		node_names[i] = node_names[i].replace(IdNumber[i],'')
+		Action.append(node_names[i])
+		node_names[i] = node_names[i].replace('Added Database Node ','')
+		data_ = {'Actions':Action,'NodeNames':node_names,'NodeIds':IdNumber}
+		d+= 1
 
-with open('NodeData.json','w') as NodeData:
-	NodeData.write(json.dumps(
-		data_,
-		indent=2,
-		sort_keys=True
-	))
-	NodeData.flush()
-	NodeData.close()
+	with open('NodeData.json','w') as NodeData:
+		NodeData.write(json.dumps(
+			data_,
+			indent=2,
+			sort_keys=True
+		))
+		NodeData.flush()
+		NodeData.close()
 
-#for i in range(len(listed_)):
-#	os.remove(listed_[i])
+	#for i in range(len(listed_)):
+	#	os.remove(listed_[i])
