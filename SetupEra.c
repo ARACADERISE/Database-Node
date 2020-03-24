272c0f9342a9aa531686f6f259425450f026ee1d
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-24 15:37:20 -0500

    Create SetupEra.c

---
+++ SetupEra.c
@@ -0,0 +1,14 @@
+/* 
+	Sets up what the ERA type does. Backend to ERAS
+*/
+
+#include <stdio.h>
+#include "types.h"
+
+// Gathers the Era name using the DefaultMainDbNode struct
+void *
+GatherEra(char *Era, DefaultMainDbNode *Eras/*Used to go through all ERAS setup through the Default Node*/) {
+
+	for(int i = 0; i < 4; i++) {}
+	return 0;
+}
