4a671d4836c30d3d274a96d39cc61a27ab5936dc
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-28 00:51:05 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main(void) {
 	// The program will go off of this
-	SetupDatabaseNode("DefaultNodeSetup", true, true, "NUN");
+	SetupDatabaseNode("DefaultNodeSetup", true, true, false, "NUN", 10000, 20000, 20000);
 	
 	// Final step of setting up Database Nodes
 	system("python Python/dataparser.py");
