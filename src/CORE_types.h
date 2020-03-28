2e501b2416149de5424cc65b3e51ed1dda6272ef
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-27 22:03:26 -0500

    Update CORE_types.h

--- src/CORE_types.h
+++ src/CORE_types.h
@@ -32,58 +32,58 @@
 // Storage Errors
 #  define StorageAboveMax                   13
 #  define StorageAddOverload                14
-#  define AllocatingStorageWithDefaultNode  15 // Counted as a warning
+#  define AllocatingStorageWithSizeZero     15
 // Regular success/fail errors
 #  define Failure                           1
 #  define Success                           0
 
-#define RETURNERRINFO(COLOR,TYPE)                                                                                                                                              \
-	if(TYPE == CoreConErr) {                                                                                                                                               \
-		printf("%sError connection with core. \n\tReturn Exit status %d\n",COLOR,CoreConErr);                                                                          \
-	}                                                                                                                                                                      \
-	else if(TYPE == FileConErr) {                                                                                                                                          \
-		printf("%sError connecting/finding file. \n\tReturn Exit status %d\n",COLOR,FileConErr);                                                                       \
-	}                                                                                                                                                                      \
-	else if(TYPE == FoundInOtherFile) {                                                                                                                                    \
-		printf("%sThe string/number/character was found in another file. \n\tReturn Exit status %d\n",COLOR,FoundInOtherFile);                                         \
-	}                                                                                                                                                                      \
-	else if(TYPE == DeclarationOfEraNun) {                                                                                                                                 \
-		printf("%sAttempt to set a Era type of NUN to your Database Node. \n\tERR_STATUS_%d\n",COLOR,DeclarationOfEraNun);                                             \
-	}                                                                                                                                                                      \
-	else if(TYPE == DefaultNodeSetupNotFound) {                                                                                                                            \
-		printf("%sThe DefaultNodeSetup Database Node was not found.\n\tERR_STATUS_%d\n",COLOR,DefaultNodeSetupNotFound);                                               \
-	}                                                                                                                                                                      \
-	else if(TYPE == DefaultNodeSetupEraTypeNotNun) {                                                                                                                       \
-		printf("%sThe Era type of the Default Database Node is required to be NUN.\n\tERR_STATUS_%d\n",COLOR,DefaultNodeSetupEraTypeNotNun);                           \
-	}                                                                                                                                                                      \
-	else if(TYPE == DefaultDbNodeNotFullySetup) {                                                                                                                          \
-		printf("%sThe Default Database Node is not fully set up yet.\n\tERR_STATUS_%d\n",COLOR,DefaultDbNodeNotFullySetup);                                            \
-	}                                                                                                                                                                      \
-	else if(TYPE == MoreThanOneDefaultNodeCreated) {                                                                                                                       \
-		printf("%sAttempted to create more than one Default Database Node.\n\tERR_STATUS_%d\n",COLOR,MoreThanOneDefaultNodeCreated);                                   \
-	}                                                                                                                                                                      \
-	else if(TYPE == DatabaseNodeAlreadyCreated) {                                                                                                                          \
-		printf("%sAttempted to create a already existing Database Node.\n\tERR_STATUS_%d\n",COLOR,DatabaseNodeAlreadyCreated);                                         \
-	}                                                                                                                                                                      \
-	else if(TYPE == NotAEraType) {                                                                                                                                         \
-		printf("%sAttempted to assign an unexisting Era type to the Database node.\n\tERR_STATUS_%d\n",COLOR,NotAEraType);                                             \
-	}                                                                                                                                                                      \
-	else if(TYPE == StorageAboveMax) {                                                                                                                                     \
-		printf("%sTotal storage ammount reached for the Database Node.\n\tERR_STATUS_%d\nReallocating memory for the Database Nodes storage...",COLOR,StorageAboveMax);\
-	}                                                                                                                                                                      \
-	else if(TYPE == StorageAddOverload) {                                                                                                                                  \
-		printf("%sAdding too much storage. Total of 50000 storage segments can be added at once.\n\tERR_STATUS_%d\n",COLOR,StorageAddOverload);                        \
-	}                                                                                                                                                                      \
-	else if(TYPE == AllocatingStorageWithDefaultNode) {                                                                                                                    \
-		printf("%sCannot allocate data from Default Database Node to User Created Node.\n\t_WARNING_STATUS_%d\n\n",COLOR,AllocatingStorageWithDefaultNode);            \
-	}                                                                                                                                                                      \
-	else if(TYPE == Failure) {                                                                                                                                             \
-		printf("%sFailed to compile successfully. \n\tReturn Exit status %d\n",COLOR,Failure);                                                                         \
-	 }                                                                                                                                                                     \
-	else if(TYPE == Success) {                                                                                                                                             \
-		printf("%sProgram compiled successfully. \n\tReturn Exit status %d\n",COLOR,Success);                                                                          \
-	}                                                                                                                                                                      \
-	else {                                                                                                                                                                 \
-		printf("%s",COLOR);                                                                                                                                            \
-		printf("Unknown return type\n");                                                                                                                               \
+#define RETURNERRINFO(COLOR,TYPE)                                                                                                                     \
+	if(TYPE == CoreConErr) {                                                                                                                      \
+		printf("%sError connection with core. \n\tReturn Exit status %d\n",COLOR,CoreConErr);                                                 \
+	}                                                                                                                                             \
+	else if(TYPE == FileConErr) {                                                                                                                 \
+		printf("%sError connecting/finding file. \n\tReturn Exit status %d\n",COLOR,FileConErr);                                              \
+	}                                                                                                                                             \
+	else if(TYPE == FoundInOtherFile) {                                                                                                           \
+		printf("%sThe string/number/character was found in another file. \n\tReturn Exit status %d\n",COLOR,FoundInOtherFile);                \
+	}                                                                                                                                             \
+	else if(TYPE == DeclarationOfEraNun) {                                                                                                        \
+		printf("%sAttempt to set a Era type of NUN to your Database Node. \n\tERR_STATUS_%d\n",COLOR,DeclarationOfEraNun);                    \
+	}                                                                                                                                             \
+	else if(TYPE == DefaultNodeSetupNotFound) {                                                                                                   \
+		printf("%sThe DefaultNodeSetup Database Node was not found.\n\tERR_STATUS_%d\n",COLOR,DefaultNodeSetupNotFound);                      \
+	}                                                                                                                                             \
+	else if(TYPE == DefaultNodeSetupEraTypeNotNun) {                                                                                              \
+		printf("%sThe Era type of the Default Database Node is required to be NUN.\n\tERR_STATUS_%d\n",COLOR,DefaultNodeSetupEraTypeNotNun);  \
+	}                                                                                                                                             \
+	else if(TYPE == DefaultDbNodeNotFullySetup) {                                                                                                 \
+		printf("%sThe Default Database Node is not fully set up yet.\n\tERR_STATUS_%d\n",COLOR,DefaultDbNodeNotFullySetup);                   \
+	}                                                                                                                                             \
+	else if(TYPE == MoreThanOneDefaultNodeCreated) {                                                                                              \
+		printf("%sAttempted to create more than one Default Database Node.\n\tERR_STATUS_%d\n",COLOR,MoreThanOneDefaultNodeCreated);          \
+	}                                                                                                                                             \
+	else if(TYPE == DatabaseNodeAlreadyCreated) {                                                                                                 \
+		printf("%sAttempted to create a already existing Database Node.\n\tERR_STATUS_%d\n",COLOR,DatabaseNodeAlreadyCreated);                \
+	}                                                                                                                                             \
+	else if(TYPE == NotAEraType) {                                                                                                                \
+		printf("%sAttempted to assign an unexisting Era type to the Database node.\n\tERR_STATUS_%d\n",COLOR,NotAEraType);                    \
+	}                                                                                                                                             \
+	else if(TYPE == StorageAboveMax) {                                                                                                            \
+		printf("%sTotal storage ammount reached for the Database Node.\n\tERR_STATUS_%d\nReallocating memory for the Database Nodes storage...",COLOR,StorageAboveMax);                              \
+	}                                                                                                                                             \
+	else if(TYPE == StorageAddOverload) {                                                                                                         \
+		printf("%sAdding too much storage. Total of 50000 storage segments can be added at once.\n\tERR_STATUS_%d\n",COLOR,StorageAddOverload); \
+	}                                                                                                                                             \
+	else if(TYPE == AllocatingStorageWithSizeZero) {\
+		printf("%sCannot allocate data of zero to your Database Node.\n\t_WARNING_STATUS_%d\n\n",COLOR,AllocatingStorageWithSizeZero); \
+	} \
+	else if(TYPE == Failure) {                                                                                                                    \
+		printf("%sFailed to compile successfully. \n\tReturn Exit status %d\n",COLOR,Failure);                                                \
+	 }                                                                                                                                            \
+	else if(TYPE == Success) {                                                                                                                    \
+		printf("%sProgram compiled successfully. \n\tReturn Exit status %d\n",COLOR,Success);                                                 \
+	}                                                                                                                                             \
+	else {                                                                                                                                        \
+		printf("%s",COLOR);                                                                                                                   \
+		printf("Unknown return type\n");                                                                                                      \
 	}
