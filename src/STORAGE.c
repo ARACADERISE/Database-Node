339da4330a1682c31bbba245d260ea35779ad90f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-28 22:18:31 -0500

    Update STORAGE.c

--- src/STORAGE.c
+++ src/STORAGE.c
@@ -68,10 +68,10 @@ UpdateStorage(DatabaseNodeset *Db,int *ToChange,int changeBy, int Maxed, int Siz
 	int AmmountLeft;
 
 	/*
-	 * Maxed: Set the Database Nodes storage to a maximum, so if the new storage amount is anywhere near Maxed,
+	 * Maxed: Set the Database Nodes storage to a maximum, so if the new storage amount is anywhere near Maxed(500 below and or above),
 	 * it rounds it up. If it's above Maxed, it rounds it  down
 
-	 *NOTE: If Maxed is greater than the Nodes total storage allowed it will throw a error
+	 *	NOTE: If Maxed is greater than the Nodes total storage allowed it will throw a error
 	*/
 
 	if(Maxed >= Db->CoreInfo.NodeStorage.MaxStorageAllowed) {
