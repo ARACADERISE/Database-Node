0a087454808f4f113cacf74c6892895cb843c07d
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-28 22:17:01 -0500

    Update STORAGE.c

--- src/STORAGE.c
+++ src/STORAGE.c
@@ -35,7 +35,6 @@ DatabaseNodeset *ResetStorage(DatabaseNodeset *Db, int SizeToIterate) {
 			}
 		}
 	}
-
 	return 0;
 }
 
@@ -66,39 +65,68 @@ AllocateData(DatabaseNodeset *Db, int SizeToIterate, const char *NodeName) {
 
 DatabaseNodeset *
 UpdateStorage(DatabaseNodeset *Db,int *ToChange,int changeBy, int Maxed, int SizeToIterate){
-
-	*ToChange += changeBy;
+	int AmmountLeft;
 
 	/*
 	 * Maxed: Set the Database Nodes storage to a maximum, so if the new storage amount is anywhere near Maxed,
 	 * it rounds it up. If it's above Maxed, it rounds it  down
+
+	 *NOTE: If Maxed is greater than the Nodes total storage allowed it will throw a error
 	*/
 
-	if(*ToChange > Maxed) {
-		int All;
-		for(int i = 0; i < SizeToIterate+1; i++) {
-			All = Db->CoreInfo.NodeStorage.MaxFileSize[i] + Db->CoreInfo.NodeStorage.MaxStringSize[i] + Db->CoreInfo.NodeStorage.MaxIntegerSize[i];
+	if(Maxed >= Db->CoreInfo.NodeStorage.MaxStorageAllowed) {
+		static int Print_ = 0; // Printing error only once
+		ErrStatus = (_CGE == 0) ? MaxedAboveTotalStorageAllowed : Failure;
+		if(Print_ == 0){
+			RETURNERRINFO("\033[0;36m", ErrStatus);
+		}
+		Print_=1;
+		Maxed = 100000;
 
-			if(All > Db->CoreInfo.NodeStorage.MaxStorageTotal[i]) {
-				ErrStatus = (_CGE == 0) ? StorageAboveMax : Failure;
-				RETURNERRINFO("\033[1;33m", ErrStatus);
+		//system("clear");
+	}
+	if(changeBy >= Db->CoreInfo.NodeStorage.MaxStorageAllowed) {
+		static int Pr = 0; // Printing error only once
+		ErrStatus = (_CGE == 0 ) ? ChangingByAboveStorageAllowed : Failure;
+		if(Pr == 0) {
+			RETURNERRINFO("\033[0;36m", ErrStatus);
+		}
+		Pr=1;
+		changeBy=Db->CoreInfo.NodeStorage.MaxStorageUpgrade;
+	}
+	*ToChange += changeBy;
 
-				Db->CoreInfo.NodeStorage.MaxFileSize[i] = 40000000/3;
-				Db->CoreInfo.NodeStorage.MaxStringSize[i] = 40000000/3;
-				Db->CoreInfo.NodeStorage.MaxIntegerSize[i]=40000000/3;
+	if(*ToChange != Db->CoreInfo.NodeStorage.MaxStorageAllowed && !(*ToChange>=Db->CoreInfo.NodeStorage.MaxStorageAllowed))
+	{
+		if(*ToChange > Maxed) {
+			int All;
+			for(int i = 0; i < SizeToIterate+1; i++) {
+				All = Db->CoreInfo.NodeStorage.MaxFileSize[i] + Db->CoreInfo.NodeStorage.MaxStringSize[i] + Db->CoreInfo.NodeStorage.MaxIntegerSize[i];
+				if(All > Db->CoreInfo.NodeStorage.MaxStorageAllowed) {
+					ErrStatus = (_CGE == 0) ? StorageAboveMax : Failure;
+					RETURNERRINFO("\033[1;33m", ErrStatus);
 
-				system("clear"); // We don't want to keep all that information printed
+					Db->CoreInfo.NodeStorage.MaxFileSize[i] = 40000000/3;
+					Db->CoreInfo.NodeStorage.MaxStringSize[i] = 40000000/3;
+					Db->CoreInfo.NodeStorage.MaxIntegerSize[i]=40000000/3;
+
+					system("clear"); // We don't want to keep all that information printed
+				}
+			}
+			
+			int Sub = Maxed-*ToChange;
+			*ToChange = *ToChange + Sub;
+		}
+		else {
+			int AmLeft;
+			if(Maxed-*ToChange <= 500) {
+				AmLeft = Maxed-*ToChange;
+				*ToChange += AmLeft;
 			}
 		}
-		
-		int Sub = Maxed-*ToChange;
-		*ToChange = *ToChange + Sub;
-	}
-	else {
-		int AmLeft;
-		if(Maxed-*ToChange <= 500) {
-			AmLeft = Maxed-*ToChange;
-			*ToChange += AmLeft;
+	} else {
+		if(*ToChange >= Db->CoreInfo.NodeStorage.MaxStorageAllowed) {
+			*ToChange = 40000000/3;
 		}
 	}
 
@@ -110,8 +138,11 @@ DatabaseNodeset *
 SetupNodeStorage(
 	DatabaseNodeset *DbNode,
 	NodeSizes *Sizes,
+	char DbNames[1000][100],
 	int SizeToIterate
 ) {
+
+	char Update[2000][150];
 
 	// Storage is factored into the Sizes struct in types.h, just not set to the Datbase Node
 	for(int i = 0; i < SizeToIterate+1; i++) {
@@ -135,8 +166,16 @@ SetupNodeStorage(
 		DbNode->CoreInfo.StorageUsed.TotalIntegerStorageUsed[i]=0;
 	}
 
+	// Signing of the value of 40000000 stored in sizes to CoreInfo.NodeStorage.MaxStorageAllowed
+	DbNode->CoreInfo.NodeStorage.MaxStorageAllowed=Sizes->MaxStorageAllowed;
+
 	// Sizes aren't needed anymore
 	free(Sizes);
+
+	// Sets ammount of storage the Node can hold for each
+	for(int i = 0; i < SizeToIterate+1; i++) {
+		sprintf(Update[i],"%s Storage ->", DbNames[i]);
+	}
 
 	return 0;
 }
