e15c68bad3a7d0f29ff634f9a624f94e578fda2b
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-10 16:20:57 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 int main(void) {
 	// The program will go off of this
 	SetupDatabaseNode("DefaultNodeSetup", true, true, false, "NUN", 0, 0, 0);
-	SetupDatabaseNode("default", true, true, false, "da", 40000, 80000, 90000);
+
+	char AddNode[4];
+	printf("Add Node?[yes/no]: ");
+	scanf("%s",AddNode);
+	if(strcmp(AddNode,"yes")==0)SetupNode();
 
 	// Final step of setting up Database Nodes
 	system("python Python/dataparser.py");
