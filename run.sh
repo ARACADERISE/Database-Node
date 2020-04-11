995f5053c91edd4cd93fd1f5bc4d078c79f50ffa
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-10 23:23:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Easy run for the application
 # Run bash run.sh to enable ./run on Linux terminal
-clang-7 -pthread -lm -o main main.c src/CORE.c src/STORAGE.c src/SetupEra.c
+clang-7 -pthread -lm -o main main.c src/CORE.c src/Node.c src/STORAGE.c src/SetupEra.c
 
 # making run runnable by typing ./run
 chmod +x run
