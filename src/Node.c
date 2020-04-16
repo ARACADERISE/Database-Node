ca14050a25b3a12cebe27b1f36ed8739fdce589b
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-16 18:55:23 -0500

    Update Node.c

--- src/Node.c
+++ src/Node.c
@@ -15,22 +15,26 @@ SetupNode() {
 
   Times++; // Increments at beggining due to the fact this is keeping track how many time the function is called
   // Ideals for the file
-  char NodeName[150], EraType[4], CGE[3], CanRead[3], AllocData[3];
+  char NodeName[150], EraType[4], CanRead[4], AllocData[3], CGE[4];
   bool __CGE,CanRead_,AlocData;
   int FileSize, StringSize, IntegerSize;
 
   printf("Node Name #%d: ",Times);scanf("%s",NodeName);
-  printf("Core Generated Errors(yes/no): ");scanf("%s",CGE);
-  printf("Can Read(yes/no): ");scanf("%s",CanRead);
-  printf("Allocated Data(yes/no): ");scanf("%s",AllocData);
-  printf("Era Type(wro(write/read), wo(write only), ro(read only), da(collect and work with data)): ");scanf("%s",EraType);
-  printf("File Size(defaults to 10000): ");scanf("%d",&FileSize);
-  printf("String Size(defaults to 20000): ");scanf("%d",&StringSize);
-  printf("Integer Size(defaults to 20000): ");scanf("%d",&IntegerSize);
+  printf("-----\nCore Generated Errors(yes/no): ");scanf("%s",CGE);
+  printf("-----\nCan Read(yes/no): ");scanf("%s",CanRead);
+  printf("-----\nAllocated Data(yes/no): ");scanf("%s",AllocData);
+  printf("-----\nEra Type(wro(write/read), wo(write only), ro(read only), da(collect and work with data)): ");scanf("%s",EraType);
+  printf("-----\nFile Size(defaults to 10000): ");scanf("%d",&FileSize);
+  printf("-----\nString Size(defaults to 20000): ");scanf("%d",&StringSize);
+  printf("-----\nInteger Size(defaults to 20000): ");scanf("%d",&IntegerSize);
 
   if(strcmp(CGE,"yes")==0)__CGE=true;
+  else __CGE=false;
   if(strcmp(CanRead,"yes")==0)CanRead_=true;
+  else CanRead_=false;
   if(strcmp(AllocData,"yes")==0)AlocData=true;
+  else if(strcmp(AllocData, "no")) AlocData=false;
+  else AlocData=false;
 
   SetupDatabaseNode(NodeName, __CGE, CanRead_, AlocData, EraType,FileSize,StringSize,IntegerSize);
   printf("\n");
